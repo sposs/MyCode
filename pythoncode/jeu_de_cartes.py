c6a5f4a3290e09274f0caf0f393a5fc801a68118
Remi Ete <remi.ete@gmail.com>
2011-08-01 16:11:15 +0200

    bug fix

--- pythoncode/jeu_de_cartes.py
+++ pythoncode/jeu_de_cartes.py
@@ -1,31 +1,43 @@
 #!/usr/bin/python
-  
+
 import os
 from random  import randrange
 
 class Joueur(object) :
-	def __init__(self) :
-		self.compte = 0
-		self.partieGagnee = 0
-	def creerJeuDeCartes(self) :
-		return self.JeuDeCartes()
+  def __init__(self) :
+    self.compte = 0
+    self.partieGagnee = 0
 
+  def creerJeuDeCartes(self) :
+    return self.JeuDeCartes()
+
+####
 class JeuDeCartes(object) :
-	def __init__(self) : 
-		self.listCartes = []
-		couleur = ("Pique","Trefle","Coeur","Carreau")
-		valeurs = (2,3,4,5,6,7,8,9,10,"valet","dame","roi","as")
-		for elt in self.listCartes :
-			for val,coul in valeurs,couleur :
-				self.listCartes.append((val,coul))
-	def nomCartes(self,(val,coul)) :
-		return "{0} de {1}".format(val,coul)
-	def battre(self) :
-		t = len(self.listCartes)
-		for elt in range(t):
-			c1,c2 = randrange(t), randrange(t)
-			self.listCartes[c1], self.listCartes[c2] = self.listCartes[c2], self.listCartes[c1]
-	def tirer(self) :
-		print "carte tirée = {0} ".format(self.nomCartes(self.listCartes[0]))
-		del self.listCartes[0]
+  def __init__(self) : 
+    self.listCartes = []
+    couleur = ("Pique","Trefle","Coeur","Carreau")
+    valeurs = (2,3,4,5,6,7,8,9,10,"valet","dame","roi","as")
+    for elt in self.listCartes :
+      for val,coul in valeurs,couleur :
+        self.listCartes.append((val,coul))
 
+  def nomCartes(self, carte) :
+    """ retourne nom de la carte
+    @param val: Valeur
+    @type val: string or int
+    
+    @return: string
+    """
+    return "%s de %s"%(carte[0],carte[1])
+
+  def battre(self) :
+    """ comment
+    """
+    t = len(self.listCartes)
+    for elt in range(t):
+      c1,c2 = randrange(t), randrange(t)
+      self.listCartes[c1], self.listCartes[c2] = self.listCartes[c2], self.listCartes[c1]
+
+  def tirer(self) :
+    print "carte tiree = %s"%self.nomCartes(self.listCartes[0])
+    del self.listCartes[0]
